a5fd5787694bea8a6435f44bb5aad49d4ef68b0c
michalchawar <michalchawar+github@gmail.com>
2025-11-05 15:52:10 +0100

    Dodanie rozszerzenia IEnumerable

---
+++ SOK.Infrastructure/Extensions/LanguageExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+
+namespace SOK.Infrastructure.Extensions
+{
+    /// <summary>
+    /// Klasa do rozszerzania funkcjonalności związanych z językiem programowania.
+    /// </summary>
+    public static class LanguageExtensions
+    {
+        /// <summary>
+        /// Przemienia zwykłe <see cref="IEnumerable"/> na <see cref="IEnumerable"/> z indeksem.
+        /// </summary>
+        /// <returns>
+        /// Obiekt <see cref="IEnumerable"/> z parami <T, int>.
+        /// </returns>
+        public static IEnumerable<(T item, int index)> WithIndex<T>(this IEnumerable<T> self)
+            => self.Select((item, index) => (item, index));
+    }
+}
